b821784c45edf91dbe4390944d0f7b9aace5b134
MartinSaposnik <martinrobertosaposnik@gmail.com>
2022-09-11 21:40:31 -0300

    add buttons

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -7,7 +7,26 @@ export default function Calculator(){
         <div className="calculatorContainer">
             <CalculatorScreen />
             <div className="container">
-
+                <Button type = 'action' value='AC' />
+                <Button type = 'operator' value='%' />
+                <Button type = 'action' value='<=' />
+                <Button type = 'operator' value='/' />
+                <Button type = 'number' value='7' />
+                <Button type = 'number' value='8' />
+                <Button type = 'number' value='9' />
+                <Button type = 'operator' value='*' />
+                <Button type = 'number' value='4' />
+                <Button type = 'number' value='5' />
+                <Button type = 'number' value='6' />
+                <Button type = 'operator' value='-' />
+                <Button type = 'number' value='1' />
+                <Button type = 'number' value='2' />
+                <Button type = 'number' value='3' />
+                <Button type = 'operator' value='+' />
+                <Button type = 'action' value='+/-' />
+                <Button type = 'number' value='0' />
+                <Button type = 'action' value='.' />
+                <Button type = 'action' value='=' />
             </div>
         </div>
     </CalculatorState>
